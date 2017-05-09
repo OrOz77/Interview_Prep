696e956c1afaeab65ffd5b17ff7b5b5b01ddc210
Or Oz <oroz77@gmail.com>
2017-05-09 18:54:45 -0400

    practice

--- CTCIv6/Linked Lists/2.5 Sum Lists/sum.java
+++ CTCIv6/Linked Lists/2.5 Sum Lists/sum.java
@@ -12,9 +12,25 @@ public ListNode addLL(ListNode h1, ListNode h2){
   ListNode result = null;
 
   while(h1.next != null && h2.next != null && !carryFlag){
-    sum = carryFlag ? 1 : 0;
-    if(h1 != null){
-      sum +=
+    sum = carryFlag ? 1 : 0;  //use carry flag
+    if(h1 != null){ //add first LL if possible
+      sum += h1.data;
+      h1 = h1.next;
     }
+    if(h2 != null){ //add second LL if possible
+      sum += h2.data;
+      h2 = h2.next;
+    }
+    if(sum > 9){  //checks for carry
+      sum = sum%10; //grab only the ones digit
+      carryFlag = true;
+    }
+    ListNode tmp = new ListNode(sum); //create resulting ListNode
+    result.next = tmp;
+    result = result.next;
+
+    sum = 0;
   }
+  //NOTE this will not return the head of the list, need to fix
+  return result;
 }
